f43e04ee8ac017163bbcf8184f915b89a7defb2c
Randomcoder-labs <hicks.gabriel98@gmail.com>
2026-05-02 11:44:11 -0500

    Commit message: Currently, the `libomnimcode_ffi` library is built as a static library, which causes issues when trying to link it with other libraries. This commit changes the build configuration to build `libomnimcode_ffi` as a shared library instead, allowing it to be linked properly with other libraries and used in a wider range of applications.

---
+++ target/release/deps/omnimcode_core-ca4cd00d6b5e6093.d
@@ -0,0 +1,20 @@
+/home/thearchitect/OMC/target/release/deps/omnimcode_core-ca4cd00d6b5e6093.d: omnimcode-core/src/lib.rs omnimcode-core/src/ast.rs omnimcode-core/src/value.rs omnimcode-core/src/parser.rs omnimcode-core/src/interpreter.rs omnimcode-core/src/runtime/mod.rs omnimcode-core/src/runtime/stdlib.rs omnimcode-core/src/circuits.rs omnimcode-core/src/evolution.rs omnimcode-core/src/circuit_dsl.rs omnimcode-core/src/optimizer.rs omnimcode-core/src/hbit.rs omnimcode-core/src/phi_pi_fib.rs omnimcode-core/src/phi_disk.rs
+
+/home/thearchitect/OMC/target/release/deps/libomnimcode_core-ca4cd00d6b5e6093.rlib: omnimcode-core/src/lib.rs omnimcode-core/src/ast.rs omnimcode-core/src/value.rs omnimcode-core/src/parser.rs omnimcode-core/src/interpreter.rs omnimcode-core/src/runtime/mod.rs omnimcode-core/src/runtime/stdlib.rs omnimcode-core/src/circuits.rs omnimcode-core/src/evolution.rs omnimcode-core/src/circuit_dsl.rs omnimcode-core/src/optimizer.rs omnimcode-core/src/hbit.rs omnimcode-core/src/phi_pi_fib.rs omnimcode-core/src/phi_disk.rs
+
+/home/thearchitect/OMC/target/release/deps/libomnimcode_core-ca4cd00d6b5e6093.rmeta: omnimcode-core/src/lib.rs omnimcode-core/src/ast.rs omnimcode-core/src/value.rs omnimcode-core/src/parser.rs omnimcode-core/src/interpreter.rs omnimcode-core/src/runtime/mod.rs omnimcode-core/src/runtime/stdlib.rs omnimcode-core/src/circuits.rs omnimcode-core/src/evolution.rs omnimcode-core/src/circuit_dsl.rs omnimcode-core/src/optimizer.rs omnimcode-core/src/hbit.rs omnimcode-core/src/phi_pi_fib.rs omnimcode-core/src/phi_disk.rs
+
+omnimcode-core/src/lib.rs:
+omnimcode-core/src/ast.rs:
+omnimcode-core/src/value.rs:
+omnimcode-core/src/parser.rs:
+omnimcode-core/src/interpreter.rs:
+omnimcode-core/src/runtime/mod.rs:
+omnimcode-core/src/runtime/stdlib.rs:
+omnimcode-core/src/circuits.rs:
+omnimcode-core/src/evolution.rs:
+omnimcode-core/src/circuit_dsl.rs:
+omnimcode-core/src/optimizer.rs:
+omnimcode-core/src/hbit.rs:
+omnimcode-core/src/phi_pi_fib.rs:
+omnimcode-core/src/phi_disk.rs:
